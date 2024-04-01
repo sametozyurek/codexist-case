27710eb5c92128e0161dde56281301bb6404caca
steyoz <samet.zyurek@gmail.com>
2024-04-01 21:17:57 +0300

    [SAMETOZYUREK] project creation.

---
+++ src/main/java/com/codexist/codexist/CodexistApplication.java
@@ -0,0 +1,13 @@
+package com.codexist.codexist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodexistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodexistApplication.class, args);
+	}
+
+}
